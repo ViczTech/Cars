16c98ef5d1528d13979cb02390529432e5dea284
Vicz <victorndukweruiz@gmail.com>
2024-06-16 20:47:22 +0100

    Day 2

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const menu = document.getElementById("menu");
-const login = document.getElementById("login");
-const sign = document.getElementById("sign-up");
-const items = document.getElementById("nav-items");
-const navBTN = document.getElementById("nav-btn");
+const menu = document.getElementById("menu"); // Menu icon element
+const login = document.getElementById("login"); // Login button element
+const sign = document.getElementById("sign-up"); // Sign-up button element
+const items = document.getElementById("nav-items"); // Navigation items container
+const navBTN = document.getElementById("nav-btn"); // Navigation button element
 
-const windowWidth = window.innerWidth;
+// Get the current window width
+// const windowWidth = window.innerWidth;
 
-console.log(windowWidth);
-
-menu.addEventListener('click', function() {
-  if (menu.classList.contains('fa-bars')) {
-      menu.classList.remove('fa-bars');
-      menu.classList.add('fa-x');
-      items.style.display = 'flex';
+// Add a click event listener to the menu icon
+menu.addEventListener("click", function () {
+  // Check if the menu icon currently has the 'fa-bars' class
+  if (menu.classList.contains("fa-bars")) {
+    // If it does, remove the 'fa-bars' class and add the 'fa-x' class
+    menu.classList.remove("fa-bars");
+    menu.classList.add("fa-x");
+    // Display the navigation items
+    items.style.display = "flex";
   } else {
-      menu.classList.add('fa-bars');
-      menu.classList.remove('fa-x');
-      items.style.display = 'none';
+    // If it doesn't, add the 'fa-bars' class and remove the 'fa-x' class
+    menu.classList.add("fa-bars");
+    menu.classList.remove("fa-x");
+    // Hide the navigation items
+    items.style.display = "none";
   }
-})
-
-
-
+});
